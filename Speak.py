4dd7a8bcc844358fe45be8220c7b416a62d00f15
Debadrito Dutta <83663034+CoderKid12344@users.noreply.github.com>
2021-10-19 20:13:35 +0530

    Add files via upload

---
+++ Speak.py
@@ -0,0 +1,11 @@
+import pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 200)
+
+def Say(audio):
+    print(audio)
+    engine.say(audio)
+    engine.runAndWait()
